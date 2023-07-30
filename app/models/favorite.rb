bc2274adf2b456d72e50cf50357eca1ba3c13bfe
Kevin Robles <kevinronu@gmail.com>
2023-07-30 00:02:37 -0500

    chore: add validations for favorite model

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :user
-  belongs_to :property
+  belongs_to :property, counter_cache: true
+
+  validates :user,
+            uniqueness: { scope: :property, message: "and Property combination already taken" }
 end
