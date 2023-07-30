06dece1fb9e8aa9f166a01c34bb8c81f9cfcaa61
Kevin Robles <kevinronu@gmail.com>
2023-07-30 11:13:24 -0500

    feat: update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,38 @@
 class UsersController < ApplicationController
-  # GET /users/:id
+  skip_before_action :require_login!, only: :create
+
+  # GET /profile
   def show
+    render json: user_data(current_user)
   end
 
-  # POST /users
+  # POST /api/users
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: user_data(@user), status: :created
+    else
+      render json: { errors: @user.errors }, status: :unprocessable_entity
+    end
   end
 
-  # PATCH/PUT /users/:id
+  # PATCH /api/profile
   def update
+    if current_user.update(user_params)
+      render json: user_data(current_user), status: :ok
+    else
+      render json: { errors: current_user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_data(user)
+    user.as_json(except: %i[password_digest])
+  end
+
+  def user_params
+    params.permit(:name, :phone, :email, :role, :password, :password_confirmation)
   end
 end
