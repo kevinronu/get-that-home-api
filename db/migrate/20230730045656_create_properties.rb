883b219bbfcde416a215c7810752caf4161b67cd
Kevin Robles <kevinronu@gmail.com>
2023-07-29 23:57:08 -0500

    chore: generate property model

---
+++ db/migrate/20230730045656_create_properties.rb
@@ -0,0 +1,25 @@
+class CreateProperties < ActiveRecord::Migration[7.0]
+  def change
+    create_table :properties do |t|
+      t.integer :operation_type
+      t.string :address
+      t.string :city
+      t.string :country
+      t.integer :price
+      t.integer :monthly_rent
+      t.integer :maintenance
+      t.integer :property_type
+      t.integer :bedrooms
+      t.integer :bathrooms
+      t.integer :area
+      t.boolean :pets_allowed
+      t.text :about
+      t.boolean :close
+      t.integer :favorites_count
+      t.integer :contacts_count
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
