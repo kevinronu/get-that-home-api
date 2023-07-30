eb5906d29ed0164d92d8149f07dd121fd836adcc
Kevin Robles <kevinronu@gmail.com>
2023-07-30 00:02:00 -0500

    chore: add validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ApplicationRecord
   has_secure_token
+  has_secure_password
+
+  has_many :properties, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :contacts, dependent: :destroy
+
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :role, presence: true
+  validates :email, presence: true, uniqueness: true
+
+  enum role: { landlord: 0, seeker: 1 }
 end
