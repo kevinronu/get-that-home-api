1dd284109b4c0ecb3ca59f726c557623956ef7c7
Kevin Robles <kevinronu@gmail.com>
2023-07-30 11:18:02 -0500

    feat: add my_properties controller

--- app/controllers/my_properties_controller.rb
+++ app/controllers/my_properties_controller.rb
@@ -1,5 +1,17 @@
 class MyPropertiesController < ApplicationController
   # GET /my_properties
   def index
+    my_properties = Property.where(user_id: current_user.id).order(created_at: :desc)
+    my_properties_data = []
+    my_properties.each do |property|
+      my_properties_data.push(property_data(property))
+    end
+    render json: my_properties_data, status: :ok
+  end
+
+  private
+
+  def property_data(property)
+    property.as_json(except: %i[user_id])
   end
 end
