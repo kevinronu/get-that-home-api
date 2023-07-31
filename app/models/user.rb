eeb202786f8860228f533d7f0fe846bfd06c94d3
Kevin Robles <kevinronu@gmail.com>
2023-07-30 22:21:58 -0500

    chore: add minimun length to passowrd

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   validates :phone, presence: true
   validates :role, presence: true
   validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, length: { minimum: 6 }
 
   enum role: { landlord: 0, seeker: 1 }
 end
