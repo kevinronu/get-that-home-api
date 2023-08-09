bcf82bc20b26f694b1a301dd9e9fef28a2218b50
Kevin Robles <kevinronu@gmail.com>
2023-08-08 22:05:41 -0500

    feat: add validation for images

--- app/models/property.rb
+++ app/models/property.rb
@@ -17,15 +17,45 @@ class Property < ApplicationRecord
   enum property_type: { apartment: 0, house: 1 }
 
   validate :validate_fields_based_on_operation_type
+  validate :validate_image_attachments
 
   private
 
   def validate_fields_based_on_operation_type
     if operation_type == "sale"
-      errors.add(:price, "must be present for sale") if price.blank?
+      validate_price_for_sale
     elsif operation_type == "rent"
-      errors.add(:monthly_rent, "must be present for rent") if monthly_rent.blank?
-      errors.add(:maintenance, "must be present for rent") if maintenance.blank?
+      validate_monthly_rent_for_rent
+      validate_maintenance_for_rent
+    end
+  end
+
+  def validate_price_for_sale
+    errors.add(:price, "must be present for sale") if price.blank?
+    errors.add(:price, "must be greater than zero") if price.present? && price <= 0
+  end
+
+  def validate_monthly_rent_for_rent
+    errors.add(:monthly_rent, "must be present for rent") if monthly_rent.blank?
+    return unless monthly_rent.present? && monthly_rent <= 0
+
+    errors.add(:monthly_rent,
+               "must be greater than zero")
+  end
+
+  def validate_maintenance_for_rent
+    errors.add(:maintenance, "must be present for rent") if maintenance.blank?
+    return unless maintenance.present? && maintenance <= 0
+
+    errors.add(:maintenance,
+               "must be greater than zero")
+  end
+
+  def validate_image_attachments
+    images.each do |image|
+      errors.add(:images, "must be image files") unless image.image?
+
+      errors.add(:images, "must be less than 5MB") if image.blob.byte_size > 5.megabytes
     end
   end
 end
