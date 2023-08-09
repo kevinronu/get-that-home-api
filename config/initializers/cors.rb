06e1d53b3b8ddd53852926d83a68901310f8e172
Kevin Robles <kevinronu@gmail.com>
2023-08-09 14:26:20 -0500

    chore: update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "get-that-home.netlify.app"
 
     resource "*",
              headers: :any,
